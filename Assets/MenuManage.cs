513d80fe7b4d451179308ad062a6492852347605
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2024-11-27 19:11:15 -0300

    Ui_ScenasDeInteracao
    Criei as cenas de interaçao do player como Start Exit Controles e creditos

---
+++ Assets/MenuManage.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManage : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game"); // Carrega a cena chamada "Game"
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits"); // Carrega a cena chamada "Credits"
+    }
+
+    public void OpenControls()
+    {
+        SceneManager.LoadScene("Controll"); // Carrega a cena chamada "Control"
+    }
+
+    public void ExitGames()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Para no editor
+#else
+        Application.Quit(); // Sai do jogo na build
+#endif
+    }
+}
