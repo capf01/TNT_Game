78e0afe4398d5886c17825ac7156537c282a9f81
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2024-12-02 15:57:26 -0300

    ScenasUiBotoesDasFases
    botoes das scenas de cada fase botao de pausar

--- Assets/windJosuel/PlayerMovement.cs
+++ Assets/windJosuel/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -25,5 +26,29 @@ public class PlayerMovement : MonoBehaviour
     public void SetWindForce(Vector2 force)
     {
         windForce = force;
+    }
+
+    // Detecta a colisão com a linha de chegada
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Verifique se o objeto que colidiu tem a tag "FinishLine"
+        if (other.CompareTag("FinishLine"))
+        {
+            // Obter o índice da cena atual
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            // Carregar o próximo nível (próximo índice)
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            // Verifica se o próximo índice existe (evita erro ao tentar carregar uma cena inexistente)
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex); // Carregar o próximo nível
+            }
+            else
+            {
+                Debug.Log("Você completou todos os níveis!"); // Ou qualquer outra lógica para o fim do jogo
+            }
+        }
     }
 }
