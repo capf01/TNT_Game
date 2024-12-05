5c948a19a8d1e4694fb184730237782f12a1b590
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2024-12-05 06:49:49 -0300

    fase5Parte2
    fase 5 -2

--- Assets/windJosuel/WindArea.cs
+++ Assets/windJosuel/WindArea.cs
@@ -1,17 +1,43 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class WindArea : MonoBehaviour
 {
     public Vector2 windForce; // Direção e força do vento
+    private List<Rigidbody2D> affectedObjects = new List<Rigidbody2D>();
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Verifica se o objeto tem um Rigidbody2D para aplicar a força
-        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-        if (rb != null && other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            // Aplica a força do vento ao Rigidbody2D
-            rb.AddForce(windForce, ForceMode2D.Force);
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            if (rb != null && !affectedObjects.Contains(rb))
+            {
+                affectedObjects.Add(rb);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            if (rb != null && affectedObjects.Contains(rb))
+            {
+                affectedObjects.Remove(rb);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Rigidbody2D rb in affectedObjects)
+        {
+            if (rb != null)
+            {
+                rb.AddForce(windForce, ForceMode2D.Force);
+            }
         }
     }
 }
