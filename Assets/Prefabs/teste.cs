9ce6487e40320980af467f2013f6b92fb29adb81
Lukas-Creat <lucasjsj2014@gmail.com>
2024-12-06 19:23:44 -0300

    sampleScene
    xfvsvk

---
+++ Assets/Prefabs/teste.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("vai na fé");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
